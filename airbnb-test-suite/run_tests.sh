7ac26665c28511a6e2769c35990909e216c94e7a
Raz <raz.sakal30@gmail.com>
2025-04-16 17:29:21 +0300

    Update run_tests.sh

--- airbnb-test-suite/run_tests.sh
+++ airbnb-test-suite/run_tests.sh
@@ -12,6 +12,6 @@ docker run --rm \
   -v "$(pwd)/.env:/app/.env" \
   --ipc=host \
   airbnb-test-suite \
-  python -m pytest tests/ "$@"
+  python -m pytest tests/
 
 echo "Tests completed! Results saved in temp directory."
